8402fd75dae584fb6a683e1b0644452202565412
hfmckenna <hfmckenna@outlook.com>
2022-06-15 22:03:57 +0100

    init

---
+++ prisma/seed.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My first note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My second note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My first post!",
+      markdown: `# This is the first post
+      
+      Pretty good for a first effort`.trim()
+    },
+    {
+      slug: "my-second-post",
+      title: "My second post!",
+      markdown: `# This is the second post
+      
+      Gone to the bother of writing two whole posts`.trim()
+    },
+  ];
+
+  for (const post of posts) {
+    await prisma.post.upsert({
+      where: { slug: post.slug },
+      update: post,
+      create: post,
+    });
+  }
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
