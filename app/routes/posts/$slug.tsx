a107c5408cfe81d84a25f2afc2e2db74bdb9452f
hfmckenna <hfmckenna@outlook.com>
2022-06-16 22:26:49 +0100

    Add invariant type check

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -3,19 +3,27 @@ import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 import { getPost } from "~/models/post.server";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+type LoaderData = {
+  title: string;
+  html: string;
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
+  invariant(slug, "slug is not found");
   const post = await getPost(slug);
+  invariant(post, `post not found: ${slug}`);
   const html = marked(post.markdown);
-  return json({ post, html });
+  return json<LoaderData>({ title: post.title, html });
 };
 
 export default function PostRoute() {
-  const { post, html } = useLoaderData();
+  const { title, html } = useLoaderData();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }}></div>
     </main>
   );
